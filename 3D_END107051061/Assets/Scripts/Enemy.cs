7c52e5f8a00a707a41662e7e12872734341ec471
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2021-01-11 07:55:34 +0800

    _(:3JZ)_

---
+++ 3D_END107051061/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        player = GameObject.Find("loli").transform;
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+        print("剩餘的距離：" + nav.remainingDistance);
+    }
+}
