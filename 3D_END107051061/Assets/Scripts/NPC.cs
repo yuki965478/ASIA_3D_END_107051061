a25a6bf1563b5ad6fc875b281d639376a087ef68
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2021-01-11 07:20:53 +0800

    程式、烘焙地形

---
+++ 3D_END107051061/Assets/Scripts/NPC.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NpcData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.1f;
+
+    public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialog,Missioning,Finish
+    }
+
+    public NPCState state = NPCState.FirstDialog;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "loli")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "loli")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+
+    private IEnumerator Dialog()
+    {
+
+        dialog.SetActive(true);
+        textContent.text = "";
+
+        textName.text = name;
+
+        string dialogString = data.dialogA;
+
+        switch (state)
+        {
+            case NPCState.FirstDialog:
+                dialogString = data.dialogA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case NPCState.Finish:
+                dialogString = data.dialogC;
+                break;
+        }
+
+        for(int i=0;i< dialogString.Length;i++)
+        {
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ 3D_END107051061/Assets/Scripts/NpcData.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NPC 資料",menuName ="Avokado/NPC 資料")]
+public class NpcData : ScriptableObject
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialogA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialogB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialogC;
+    [Header("[任務項目需求數量")]
+    public int count;
+    [Header("已取得任務數量")]
+    public int countCurrent;
+
+
+}
