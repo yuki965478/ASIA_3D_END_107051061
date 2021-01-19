0df3148189e3d9391db0ce26ee29c9ee4d5befae
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2021-01-19 12:19:08 +0800

    完成OWO/

---
+++ 3D_END107051061/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
